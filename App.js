f66e9f85661c18967474ffe0ae9e56dd3f0b47d6
Ajitesh Mandal (Nokia) <ajitesh.mandal@nokia.com>
2023-07-22 12:11:32 +0530

    Series 1

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading =React.createElement("h1",{id:"Head"},"Hello World");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(heading);
