1475913277de133798ab19400003910e26c6f0de
Ajitesh Mandal <ajiteshmandal@gmail.com>
2023-07-28 15:41:30 +0530

    written few lines using JSX & function component

--- App.js
+++ App.js
@@ -1,3 +1,19 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
-
+const Title =()=>(
+    <h1 className="head">
+        component learning with JSX
+    </h1>
+);
+const HeadComponent = ()=>(
+    <div id="container">
+        <Title/>
+        <h1>This is component learning</h1>
+       
+    </div>
+    
+);
+console.log(<HeadComponent/>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(<HeadComponent/>);
