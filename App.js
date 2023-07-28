d1c4f33b4ada7b558edc7a9a2598e37c588b7416
Ajitesh Mandal <ajiteshmandal@gmail.com>
2023-07-28 15:26:00 +0530

    introducing functional component

--- App.js
+++ App.js
@@ -1,17 +1,3 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
-/*const heading =React.createElement("h1",{id:"heading"},"Hello World");
-console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-root.render(heading);*/
 
-//JSX Element=>Parcel=>Babel
-//JSX->React.CreateElemt=>ReactElement-JS Object=>HtmlElemnt()
-const jsxheading =(<h1 className="heading">
-     Jsx Learning 
-     </h1>)
-const root=ReactDOM.createRoot(document.getElementById("root"));
-console.log(jsxheading);
-root.render(jsxheading);
-//heading & jsxHeading is same thing , check console output
